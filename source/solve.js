27bb24beaa0b315e4fdb9c70ab793b6bc12bf4c0
Oleg Schwann <cup.of.software.code@gmail.com>
2018-09-02 04:51:58 +0300

    Implemented and tested solve.js

---
+++ source/solve.js
@@ -0,0 +1,26 @@
+/*
+Напишите функцию `solve`, которая принимает на вход строку с математическим выражением,
+зависящим от переменной x и значение этой переменной, а возвращает вычисленное значение этого выражения.
+В выражении фигурируют операции сложения, вычитания, умножения. Используются только целые числа и скобки.
+
+Если выражение содержит только допустимые символы 0123456789+-*x() и нет использования x как функции:
+    создаём функцию от x с переданным выражением в виде тела
+    возвращаем результат вызова этой функции от х
+иначе кидаем ошибку распознования выражения.
+
+Можно было и без eval(), но алгоритм сортировочной станции я уже писал.
+'https://github.com/OlegSchwann/course_C/blob/master/Задача B-6 калькулятор для строк/main.c'
+Xотелось решить за наименьшее число строк кода.
+*/
+
+"use strict";
+
+const solve = (expression, x) => {
+    if (!/^[\d\-+*x()\s]+$/.test(expression))
+        throw SyntaxError("Only 0123456789+-*x() characters can be in an expression, but got " + expression);
+    if (/x\d*\s*\(/.test(expression))
+        throw SyntaxError("'x' ca't be a function, but got " + expression);
+    if (typeof(x) !== 'number' && typeof(x) !== 'undefined')
+        throw SyntaxError("'x' mast be a number or undefined, got " + typeof(x) + ".");
+    return Function("x", "return " + expression + ";")(x);
+};
--- tests/solve.js
+++ tests/solve.js
@@ -1,12 +1,37 @@
 'use strict';
 
-QUnit.module('Тестируем функцию solve', function () {
-	QUnit.test('solve работает правильно ', function (assert) {
-		assert.strictEqual(solve('x + 1', 1), 2);
-		assert.strictEqual(solve('2 + x - 1', 5), 6);
-		assert.strictEqual(solve('2 * x - 1', 5), 9);
-		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
-		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
-		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
-	});
+QUnit.module('Тестируем функцию solve', () => {
+    QUnit.test("Возращает корректное простое значение.", (assert) => {
+        assert.strictEqual(solve("1"), 1, "solve('1', x=1) === 1");
+    });
+
+    QUnit.test("Корректное вычисление простых выражений.", (assert) => {
+        assert.strictEqual(solve("2 + 2"), 4, "solve('2 + 2') === 4");
+        assert.strictEqual(solve("4 + 2 - 3 * 10"), -24, "solve('4 + 2 - 3 * 10') === -24");
+    });
+
+    QUnit.test("Корректная работа со скобками.", (assert) => {
+        assert.strictEqual(solve("(1 + 5) * (8 - 3)"), 30, "solve('(1 + 5) * (8 - 3)') == 30");
+        assert.strictEqual(solve("((1) + ((2)) * ((3) + (4)))"), 15, "solve(((1) + ((2)) * ((3) + (4)))) === 15");
+    });
+
+    QUnit.test("Работа с вычислением 'х'.", (assert) => {
+        assert.strictEqual(solve("x + 1", 1), 2, "solve('x + 1', 1) === 2");
+        assert.strictEqual(solve("2 + x - 1", 5), 6, "solve('2 + x - 1', 5) === 6");
+        assert.strictEqual(solve("2 * x - 1", 5), 9, "solve('2 * x - 1', 5) === 9");
+        assert.strictEqual(solve("2 * ( x - 1 )", 5), 8, "solve('2 * ( x - 1 )', 5) === 8");
+        assert.strictEqual(solve("(5 - x) * (x + 5)", 3), 16, "solve('(5 - x) * (x + 5)', 3) === 16");
+        assert.strictEqual(solve("((5 - x) * (x + 5)) * x * x", 3), 144, "solve('((5 - x) * (x + 5)) * x * x', 3) === 144");
+    });
+
+    QUnit.test("Работает правильно со специальными константами", (assert) => {
+        assert.strictEqual(solve("x + 23", Infinity), Infinity, "solve('x + 23') === Infinity");
+        assert.strictEqual(solve("-x-1000000000", -Infinity), Infinity, "solve('-x-1000000000', -Infinity) === Infinity");
+        assert.deepEqual(solve("x + 124", NaN), NaN, "solve('x + 124', NaN) === NaN");
+    });
+
+    QUnit.test("Отлавливает попытки инъекции JavaScript кода.", (assert) => {
+        assert.throws(solve.bind(this, "Math.abs(x)", -1), SyntaxError, "Выпонен javascript.");
+        assert.throws(solve.bind(this, "x(-1)", Math.abs), SyntaxError, "Выпонен javascript.");
+    });
 });
