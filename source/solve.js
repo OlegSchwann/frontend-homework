efcf1f488a39522b52b29bdb2b3f60e71dd12644
Oleg Schwann <cup.of.software.code@gmail.com>
2018-09-07 12:13:27 +0300

    Corrected RegExp according to naming convention.

--- source/solve.js
+++ source/solve.js
@@ -15,14 +15,14 @@ Xотелось решить за наименьшее число строк к�
 
 "use strict";
 
-let AllowedSymbols = RegExp(/^[\d\-+*x()\s]+$/);
-let FunctionCalls = RegExp(/x\d*\s*\(/);
+const allowedSymbols = RegExp(/^[\d\-+*x()\s]+$/);
+const functionCalls = RegExp(/x\d*\s*\(/);
 
 const solve = (expression, x) => {
-    if (!AllowedSymbols.test(expression)) {
+    if (!allowedSymbols.test(expression)) {
         throw SyntaxError(`Only 0123456789+-*x() characters can be in an expression, but got ${expression}.`);
     }
-    if (FunctionCalls.test(expression)) {
+    if (functionCalls.test(expression)) {
         throw SyntaxError(`'x' ca't be a function, but got ${expression}.`);
     }
     if (typeof(x) !== 'number' && typeof(x) !== 'undefined') {
